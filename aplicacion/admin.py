448d8e564f936a09009a4423444fd42d73a9fee9
bayrom <ba.caamano@duocuc.cl>
2024-06-06 10:12:47 -0400

    modelo usuario y agregao de media(para imagen) en settings.py

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import usuario
 
 # Register your models here.
+class AdmUsuario(admin.ModelAdmin):
+        list_display=['id','nombre','apellido','email','telefono','direccion']
+
+
+admin.site.register(usuario, AdmUsuario)
--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class usuario(models.Model):
+    id = models.CharField(max_length=8, primary_key=True, unique=True)
+    nombre = models.CharField(max_length=50, null=False)
+    apellido = models.CharField(max_length=50, null=False)
+    email = models.EmailField(max_length=100, null=False)
+    telefono = models.CharField(max_length=9, null=False)
+    direccion = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return f"{self.id} - {self.nombre} {self.apellido}"
